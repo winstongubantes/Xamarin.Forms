a9db06a93dfbebda2bd905d3cf9e7e8014350923
Winston Gubantes <winston.gubantes@gluonconsulting.com>
2017-02-10 22:51:02 +0800

    initiail commit

---
+++ CustomPopupDialog/CustomPopupDialog/CustomPopupDialog/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CustomPopupDialog
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            BtnShowDialog.Clicked += BtnShowDialog_Clicked;
+        }
+
+        private void BtnShowDialog_Clicked(object sender, EventArgs e)
+        {
+            PopUpDialog.ShowDialog();
+        }
+    }
+}
---
+++ CustomPopupDialog/CustomPopupDialog/CustomPopupDialog/Views/PopUpDialogView.xaml.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace CustomPopupDialog.Views
+{
+    [ContentProperty("DialogContent")]
+    public partial class PopUpDialogView : ContentView
+    {
+        public event EventHandler DialogClosed;
+        public event EventHandler DialogShow;
+        public event EventHandler DialogClosing;
+        public event EventHandler DialogShowing;
+        public static readonly BindableProperty HeaderTitleProperty = BindableProperty.Create("HeaderTitle", typeof(string), typeof(PopUpDialogView), string.Empty, BindingMode.TwoWay);
+
+        public string HeaderTitle
+        {
+            get { return (string)GetValue(HeaderTitleProperty); }
+            set { SetValue(HeaderTitleProperty, value); }
+        }
+
+        public PopUpDialogView()
+        {
+            InitializeComponent();
+
+            PopUpBgLayout.GestureRecognizers.Add(new TapGestureRecognizer
+            {
+                Command = new Command(HideDialog)
+            });
+
+            PopUpDialogClose.GestureRecognizers.Add(new TapGestureRecognizer
+            {
+                Command = new Command(HideDialog)
+            });
+        }
+
+        public void ShowDialog()
+        {
+            ShowDialogAnimation(PopUpDialogLayout, PopUpBgLayout);
+        }
+
+        public void HideDialog()
+        {
+            HideDialogAnimation(PopUpDialogLayout, PopUpBgLayout);
+        }
+
+        public View DialogContent
+        {
+            get { return ContentView.Content; }
+            set { ContentView.Content = value; }
+        }
+
+        protected virtual void OnDialogClosed(EventArgs e)
+        {
+            DialogClosed?.Invoke(this, e);
+        }
+
+        protected virtual void OnDialogShow(EventArgs e)
+        {
+            DialogShow?.Invoke(this, e);
+        }
+
+        protected virtual void OnDialogClosing(EventArgs e)
+        {
+            DialogClosing?.Invoke(this, e);
+        }
+
+        protected virtual void OnDialogShowing(EventArgs e)
+        {
+            DialogShowing?.Invoke(this, e);
+        }
+
+        private void ShowDialogAnimation(VisualElement dialog, VisualElement bg)
+        {
+            dialog.TranslationY = bg.Height;
+            bg.IsVisible = true;
+            dialog.IsVisible = true;
+
+            ////ANIMATIONS 
+            var showBgAnimation = OpacityAnimation(bg, 0, 0.5);
+            var showDialogAnimation = TransLateYAnimation(dialog, bg.Height, 0);
+
+            ////EXECUTE ANIMATIONS
+            this.Animate("showBg", showBgAnimation, 16, 200, Easing.Linear, (d, f) => { });
+            this.Animate("showMenu", showDialogAnimation, 16, 200, Easing.Linear, (d, f) =>
+            {
+                OnDialogShow(new EventArgs());
+            });
+
+            OnDialogShowing(new EventArgs());
+        }
+
+        private void HideDialogAnimation(VisualElement dialog, VisualElement bg)
+        {
+            ////ANIMATIONS     
+            var hideBgAnimation = OpacityAnimation(bg, 0.5, 0);
+            var showDialogAnimation = TransLateYAnimation(dialog, 0, bg.Height);
+
+            ////EXECUTE ANIMATIONS
+            this.Animate("hideBg", hideBgAnimation, 16, 200, Easing.Linear, (d, f) => { });
+            this.Animate("hideMenu", showDialogAnimation, 16, 200, Easing.Linear, (d, f) =>
+            {
+                bg.IsVisible = false;
+                dialog.IsVisible = false;
+                dialog.TranslationY = PopUpBgLayout.Height;
+
+                OnDialogClosed(new EventArgs());
+            });
+
+            OnDialogClosing(new EventArgs());
+        }
+
+        private static Animation TransLateYAnimation(VisualElement element, double from, double to)
+        {
+            return new Animation(d => { element.TranslationY = d; }, from, to);
+        }
+
+        private static Animation TransLateXAnimation(VisualElement element, double from, double to)
+        {
+            return new Animation(d => { element.TranslationX = d; }, from, to);
+        }
+
+        private static Animation OpacityAnimation(VisualElement element, double from, double to)
+        {
+            return new Animation(d => { element.Opacity = d; }, from, to);
+        }
+    }
+}
